6d0b6bc7d9197c48ec1402accf0fa8c28f338024
nrai14 <nishadrai14@gmail.com>
2023-02-22 23:06:26 +0000

    typos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import inquirer from "inquirer";
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+
 const path = require("path");
 const fs = require("fs");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -29,11 +32,36 @@ inquirer
       name: "role",
       message: "What is the team member's role?",
       choices: ["Engineer", "Intern", "Manager"]
-  },{
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is their github username?",
+    when: function(answers) {
+      return answers.role === "Engineer";
+    }
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is their office number?",
+    when: function(answers) {
+      return answers.role === "Manager";
+    }
+  },
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the school the intern went to?",
+    when: function(answers) {
+      return answers.role === "Intern";
+    }
+  },
+  {
     type: "input",
     name: "email",
     message: "What is the team member's email?"
 },
-  ])
-
-  console.log()
+]).then(function(answers) {
+    console.log(answers);
+  });
